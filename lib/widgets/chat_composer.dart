82ba0dba03f1d8398f7eee6beee7e8b16c85ef13
rafidkotta <getrafid@gmail.com>
2022-02-04 13:12:49 +0400

    personal chat room UI completed

---
+++ lib/widgets/chat_composer.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+typedef OnMessage = Function(String message);
+Container buildChatComposer(OnMessage onMessage) {
+  TextEditingController editingController = TextEditingController();
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 10),
+    color: Colors.white,
+    height: 80,
+    child: Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 14),
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Type your message ...',
+                      hintStyle: TextStyle(color: Colors.grey[500]),
+                    ),
+                    controller: editingController,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        InkWell(
+          onTap: (){
+            onMessage(editingController.text);
+            editingController.clear();
+          },
+          child: const CircleAvatar(
+            radius: 25,
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
---
+++ lib/widgets/conversation.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ws_chat/controller/web_socket_controller.dart';
+import 'package:flutter_ws_chat/utils.dart';
+import 'package:provider/src/provider.dart';
+import '../providers/chat_room_provider.dart';
+
+import '../app_theme.dart';
+
+class Conversation extends StatefulWidget {
+  const Conversation({Key? key,required this.position}) : super(key: key);
+
+  final int position;
+
+  @override
+  State<Conversation> createState() => _ConversationState();
+}
+
+class _ConversationState extends State<Conversation> {
+  final ScrollController _controller = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    var room = context.watch<ChatRoom>().rooms[widget.position];
+    return ListView.builder(
+        reverse: false,
+        controller: _controller,
+        itemCount: room.messages!.length,
+        itemBuilder: (context, int index) {
+          final message = room.messages![index];
+          bool isMe = message.sender!.username == SocketController.username;
+          return Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment:
+                  isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    if (!isMe)
+                      CircleAvatar(
+                        radius: 15,
+                        child: Text(room.messages![index].sender!.username!.split("")[0].toUpperCase()),
+                      ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width * 0.6),
+                      decoration: BoxDecoration(
+                          color: isMe ? Colors.green[300] : Colors.grey[200],
+                          borderRadius: BorderRadius.only(
+                            topLeft: const Radius.circular(16),
+                            topRight: const Radius.circular(16),
+                            bottomLeft: Radius.circular(isMe ? 12 : 0),
+                            bottomRight: Radius.circular(isMe ? 0 : 12),
+                          )),
+                      child: Text(
+                        room.messages![index].message!,
+                        style: MyTheme.bodyTextMessage.copyWith(
+                            color: isMe ? Colors.white : Colors.grey[800]),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: Row(
+                    mainAxisAlignment:
+                    isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                    children: [
+                      if (!isMe)
+                        const SizedBox(
+                          width: 40,
+                        ),
+                      Icon(
+                        Icons.done_all,
+                        size: 20,
+                        color: MyTheme.bodyTextTime.color,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        localTime(message.time!),
+                        style: MyTheme.bodyTextTime,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  void _scrollDown() {
+    _controller.animateTo(
+      _controller.position.maxScrollExtent,
+      duration: Duration(seconds: 2),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+}
