7cdadcfc9bcc8770d767932ef4f15c42a4907ffb
rafidkotta <getrafid@gmail.com>
2022-02-10 18:18:32 +0400

    conversation UI improvements

--- lib/widgets/conversation.dart
+++ lib/widgets/conversation.dart
@@ -26,88 +26,90 @@ class _ConversationState extends State<Conversation> {
     return ScrollablePositionedList.builder(
         reverse: false,
         itemScrollController: _controller,
-        shrinkWrap: true,
+        shrinkWrap: false,
         itemCount: room.messages!.length,
         initialScrollIndex: room.messages!.length,
         itemBuilder: (context, int index) {
-          final message = room.messages![index];
-          bool isMe = message.sender!.username == SocketController.username;
-          Widget _chat = Column(
-            children: [
-              Row(
-                mainAxisAlignment:
-                isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
+          if(room.messages!.isNotEmpty){
+            final message = room.messages![index];
+            bool isMe = message.sender!.username == SocketController.username;
+            return Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: Column(
                 children: [
-                  if (!isMe && room.type != "personal")
-                    CircleAvatar(
-                      radius: 15,
-                      child: Text(room.messages![index].sender!.username!.split("")[0].toUpperCase()),
-                    ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    constraints: BoxConstraints(
-                        maxWidth: MediaQuery.of(context).size.width * 0.6),
-                    decoration: BoxDecoration(
-                        color: isMe ? Colors.green[300] : Colors.grey[200],
-                        borderRadius: BorderRadius.only(
-                          topLeft: const Radius.circular(16),
-                          topRight: const Radius.circular(16),
-                          bottomLeft: Radius.circular(isMe ? 12 : 0),
-                          bottomRight: Radius.circular(isMe ? 0 : 12),
-                        )),
-                    child: Text(
-                      room.messages![index].message!,
-                      style: MyTheme.bodyTextMessage.copyWith(
-                          color: isMe ? Colors.white : Colors.grey[800]),
-                    ),
-                  ),
+                  Row(
+                    mainAxisAlignment:
+                    isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      if (!isMe && room.type != "personal")
+                        CircleAvatar(
+                          radius: 15,
+                          child: Text(room.messages![index].sender!.username!.split("")[0].toUpperCase()),
+                        ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.6),
+                        decoration: BoxDecoration(
+                            color: isMe ? Colors.green[300] : Colors.grey[200],
+                            borderRadius: BorderRadius.only(
+                              topLeft: const Radius.circular(16),
+                              topRight: const Radius.circular(16),
+                              bottomLeft: Radius.circular(isMe ? 12 : 0),
+                              bottomRight: Radius.circular(isMe ? 0 : 12),
+                            )),
+                        child: Text(
+                          room.messages![index].message!,
+                          style: MyTheme.bodyTextMessage.copyWith(
+                              color: isMe ? Colors.white : Colors.grey[800]),
+                        ),
+                      ),
 
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Row(
+                      mainAxisAlignment:
+                      isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                      children: [
+                        if (!isMe)
+                          const SizedBox(
+                            width: 40,
+                          ),
+                        if (isMe) Icon(
+                          Icons.done_all,
+                          size: 20,
+                          color: MyTheme.bodyTextTime.color,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          localTime(message.time!),
+                          style: MyTheme.bodyTextTime,
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Row(
-                  mainAxisAlignment:
-                  isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-                  children: [
-                    if (!isMe)
-                      const SizedBox(
-                        width: 40,
-                      ),
-                    if (isMe) Icon(
-                      Icons.done_all,
-                      size: 20,
-                      color: MyTheme.bodyTextTime.color,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      localTime(message.time!),
-                      style: MyTheme.bodyTextTime,
-                    )
-                  ],
-                ),
-              )
-            ],
-          );
-          return Container(
-            margin: const EdgeInsets.only(top: 10),
-            child: _chat,
-          );
+            );
+          }else {
+            return Container();
+          }
         });
   }
 
   void _scrollDown(int position) async {
-    await Future.delayed(const Duration(microseconds: 500));
+    await Future.delayed(const Duration(microseconds: 1000));
     _controller.scrollTo(
       index: position,
-      duration: const Duration(seconds: 1),
-      curve: Curves.linearToEaseOut,
+      duration: const Duration(milliseconds: 500),
     );
   }
 }
